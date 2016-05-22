e4d00cbc84f24a2f8a13c0a20d6848e69f25e9fc
Abhishek Dey <adey@innominds.com>
2016-05-22 16:45:27 +0530

    Drop down working

--- angular-table/MainCtrl.js
+++ angular-table/MainCtrl.js
@@ -5,38 +5,42 @@ var app = angular.module('app', ['ui.grid', 'ui.grid.selection', 'ui.grid.groupi
 
 app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.predLevel1Alternative = [];
+    var Level1Key = 0;
     $scope.predLevel2Alternative = [];
     $scope.predLevel3Alternative = [];
     $scope.predLevel4Alternative = [];
     $scope.gridOptions = {};
-    $scope.gridOptions.data = [];
 
-    $scope.getCategoryLevelDropDown = function (predLevelAlternative, predLevelArray, predLevel) {
-        console.log(predLevelAlternative);
+    $scope.getCategoryLevelDropDown = function (predLevelAlternative, predLevelArray, predLevel, levelName) {
         var tempArray = predLevelAlternative.replace(/[\[\]'"]+/g, '');
         var position;
         tempArray = tempArray.split(',');
         angular.forEach(tempArray, function (tempData) {
+            var flag = false;
             if (predLevelArray.length > 0) {
                 angular.forEach(predLevelArray, function (predLevelData, key) {
-                    if (tempData !== predLevelData.size) {
-                        var size = predLevelArray.length;
-                        predLevelArray.push({size: tempData});
+                    if (tempData === predLevelData[levelName]) {
+                        flag = true;
                     }
-
-                    if (predLevel === predLevelData) {
+                    if (predLevel === predLevelData[levelName]) {
                         position = key;
                     }
                 });
+
+                if (flag === false) {
+                    predLevelArray.push({id: tempData});
+                    predLevelArray[predLevelArray.length - 1][levelName] = tempData;
+                }
             }
             else {
-                predLevelArray.push({0: predLevel});
-                var size = predLevelArray.length;
-                predLevelArray.push({size: tempData});
-                position = 0;
+                predLevelArray.push({id: predLevel});
+                predLevelArray[predLevelArray.length - 1][levelName] = predLevel;
+                predLevelArray.push({id: tempData});
+                predLevelArray[predLevelArray.length - 1][levelName] = tempData;
+                position = 1;
             }
         });
-        return position;
+        return predLevel;
     };
 
     $scope.createGridData = function (data) {
@@ -46,20 +50,10 @@ app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
         tempData.transactionDetails = data.transactionDetails;
         tempData.amountSpent = parseInt(data.amountSpent);
         tempData.gcsatCompliance = data.gcsatCompliance;
-        //tempData.predLevel1 = $scope.getCategoryLevelDropDown(data.predLevel1Alternative, $scope.predLevel1Alternative, data.predLevel1);
-        //tempData.predLevel2 = $scope.getCategoryLevelDropDown(data.predLevel2Alternative, $scope.predLevel2Alternative, data.predLevel2);
-        //tempData.predLevel3 = $scope.getCategoryLevelDropDown(data.predLevel3Alternative, $scope.predLevel3Alternative, data.predLevel3);
-        //tempData.predLevel4 = $scope.getCategoryLevelDropDown(data.predLevel4Alternative, $scope.predLevel4Alternative, data.predLevel4);
-        //tempData.predLevel1 = data.predLevel1;
-        if (data.predLevel1 === 'Information Technology') {
-            tempData.predLevel1 = 2;
-        } else {
-            tempData.predLevel1 = 1;
-        }
-
-        tempData.predLevel2 = data.predLevel2;
-        tempData.predLevel3 = data.predLevel3;
-        tempData.predLevel4 = data.predLevel4;
+        tempData.predLevel1 = $scope.getCategoryLevelDropDown(data.predLevel1Alternative, $scope.predLevel1Alternative, data.predLevel1, 'predLevel1');
+        tempData.predLevel2 = $scope.getCategoryLevelDropDown(data.predLevel2Alternative, $scope.predLevel2Alternative, data.predLevel2, 'predLevel2');
+        tempData.predLevel3 = $scope.getCategoryLevelDropDown(data.predLevel3Alternative, $scope.predLevel3Alternative, data.predLevel3, 'predLevel3');
+        tempData.predLevel4 = $scope.getCategoryLevelDropDown(data.predLevel4Alternative, $scope.predLevel4Alternative, data.predLevel4, 'predLevel4');
         $scope.gridOptions.data.push(tempData);
     };
 
@@ -71,7 +65,7 @@ app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
                 enableFiltering: true,
                 treeRowHeaderAlwaysVisible: false,
                 columnDefs: [
-                    {name: 'glAccountDescription',display:'desc', width: '30%'},
+                    {name: 'glAccountDescription', display: 'desc', width: '30%'},
                     {name: 'supplierName', width: '10%'},
                     {name: 'transactionDetails', width: '10%'},
                     {name: 'amountSpent', width: '5%'},
@@ -80,19 +74,33 @@ app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
                         name: 'predLevel1',
                         displayName: 'PredLevel1',
                         editableCellTemplate: 'ui-grid/dropdownEditor',
-                        width: '20%',
-                        cellFilter: 'mapPredLevel1',
+                        width: '10%',
                         editDropdownValueLabel: 'predLevel1',
-                        editDropdownOptionsArray: [
-                            {id: 0, predLevel1: 'Business Services'},
-                            {id: 1, predLevel1: 'Non-Sourceable'},
-                            {id: 2, predLevel1: 'Information Technology'}
-                        ]
+                        editDropdownOptionsArray: $scope.predLevel1Alternative
                     },
-                    {name: 'predLevel2', width: '10%'},
-                    {name: 'predLevel3', width: '10%'},
-                    {name: 'predLevel4', width: '10%'}
-                ]
+                    {
+                        name: 'predLevel2', displayName: 'PredLevel2',
+                        editableCellTemplate: 'ui-grid/dropdownEditor',
+                        width: '10%',
+                        editDropdownValueLabel: 'predLevel2',
+                        editDropdownOptionsArray: $scope.predLevel2Alternative
+                    },
+                    {
+                        name: 'predLevel3', displayName: 'PredLevel3',
+                        editableCellTemplate: 'ui-grid/dropdownEditor',
+                        width: '10%',
+                        editDropdownValueLabel: 'predLevel3',
+                        editDropdownOptionsArray: $scope.predLevel3Alternative
+                    },
+                    {
+                        name: 'predLevel4',displayName: 'PredLevel4',
+                        editableCellTemplate: 'ui-grid/dropdownEditor',
+                        width: '10%',
+                        editDropdownValueLabel: 'predLevel4',
+                        editDropdownOptionsArray: $scope.predLevel4Alternative
+                    }
+                ],
+                data: []
             };
             var parentIncrement, incrementId = 0;
             angular.forEach($scope.classificationList, function (data) {
@@ -111,68 +119,8 @@ app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
                     else if (key > 0 && data.length > 1) {
                         $scope.gridOptions.data[parentIncrement].amountSpent += parseInt($scope.gridOptions.data[incrementId].amountSpent);
                     }
-
                     incrementId++;
                 });
             });
         });
 }]);
-
-app.filter('mapPredLevel1', function () {
-    var predLevel1Hash = {
-        0: 'Business Services',
-        1: 'Non-Sourceable',
-        2: 'Information Technology'
-    };
-
-    return function (input) {
-        if (!input) {
-            return '';
-        } else {
-            return predLevel1Hash[input];
-        }
-    }
-});/*
-app.filter('mapPredLevel2', function () {
-    var predLevel2Hash = {
-        0: 'Temporary Labor',
-        1: 'Logistics, Transportation & Freight Equipment'
-    };
-
-    return function (input) {
-        if (!input) {
-            return '';
-        } else {
-            return predLevel2Hash[input];
-        }
-    }
-});
-app.filter('mapPredLevel3', function () {
-
-    var predLevel3Hash = {
-        0: 'IT Temporary Labor',
-        1: 'Lifting, Handling, Loading & Unloading Equipment'
-    };
-
-    return function (input) {
-        if (!input) {
-            return '';
-        } else {
-            return predLevel3Hash[input];
-        }
-    }
-});
-app.filter('mapPredLevel4', function () {
-    var predLevel1Map = {
-        0: 'IT Temporary Labor',
-        1: 'Lifting, Handling, Loading & Unloading Equipment'
-    };
-
-    return function (input) {
-        if (!input) {
-            return '';
-        } else {
-            return predLevel1Map[input];
-        }
-    }
-});*/
